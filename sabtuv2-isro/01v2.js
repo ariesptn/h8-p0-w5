ff4110ab1b24e54726f36ffc6c5a347997ab4d86
Arie Septian <ariesptn@gmail.com>
2018-12-15 16:43:45 +0700

    added sabtuv2isro

---
+++ sabtuv2-isro/01v2.js
@@ -0,0 +1,39 @@
+/*
+================
+Eja Uang
+================
+
+[INSTRUCTIONS]
+
+formatUang adalah sebuah function yang menerima satu parameter berupa number.
+function akan mereturn sebuah sebuah string yang merupakan pengejaan uang seperti:
+1.500 => 1 ribu 5 ratus
+
+[RULE]
+- Wajib menggunakan Pseudocode/Algoritma
+- Hanya boleh menggunakan sintaks for/while, if-else, serta operasi array untuk pemecahan masalah.
+- Dilarang menggunakan regex .match dan lainnya!
+
+[EXAMPLE]
+formatUang(2000)
+
+output: 2 ribu
+*/
+
+/*
+
+
+
+*/
+
+
+function formatUang(number) {
+
+}
+  
+console.log(formatUang(7500)); // 7 ribu 5 ratus
+console.log(formatUang(250)); // 2 ratus 5 puluh
+console.log(formatUang(100000)); // 100 ribu
+console.log(formatUang(1000000)); // 1 juta
+console.log(formatUang(4999999)); // 4 juta 999 ribu 9 ratus 9 puluh 9
+console.log(formatUang(13500)); // 135 ribu
---
+++ sabtuv2-isro/02v2.js
@@ -0,0 +1,20 @@
+/*
+==============
+bigIt Function
+==============
+Instruksi
+=========
+Ubah seiap huruf depan menjadi paling belakang posisinya dan ditambah dengan imbuhayn -ay
+Contoh:
+Big => igBay
+RULES:
+- Dilarang menggunakan .split .map .filter .sort .reduce
+- Dilarang menggunakan REG EX
+- WAJIB MENGGUNAKAN PSEUDOCODE 
+*/
+function bigIt(array){
+
+}
+
+console.log(bigIt('Big latin is cool')); //'igBay atinlay siay oolcay'
+console.log(bigIt('This is my string')); //'hisTay siay ymay tringsay'
